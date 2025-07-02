27ffa27903054a3c2845ad151049fded86d5fb41
AndrewDoan01 <23520006@gm.uit.edu.vn>
2025-07-02 11:04:53 +0700

    first index.js file

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const http = require('http');
 const server = http.createServer((req, res) => {
     // Set the response header
     res.writeHead(200, { 'Content-Type': 'text/html' });
-    
+
     // Send a response
     res.end('<h1>Hello World!</h1><p>This is a basic Node.js server.</p>');
 });
